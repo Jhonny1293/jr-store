b1c33fa8411f18f8fe258f921f1307d88192a12a
Jhonatan <jhona_re_mo@hotmail.com>
2023-07-20 03:10:04 -0500

    feat: implement views

---
+++ domain/controller/products/delete-controller.js
@@ -0,0 +1,16 @@
+import services from "../../services/products-services.js";
+
+export function deleteProduct(id) {
+    if(!id){
+      alert('ha ocurrido un error');
+      return;
+    }
+
+    services.deleteProduct(id)
+    .then((respuesta) => {
+        window.location.href = '/pages/products.index.html';
+    }).catch(error => {
+        alert(error)
+    }); 
+
+}
